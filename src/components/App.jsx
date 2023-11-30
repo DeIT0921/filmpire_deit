451ae183ea345d21a79c8c0dc33fce84b5c273ba
DeIT0921 <danarikram@gmail.com>
2023-11-30 11:40:54 +0700

    Structure file and routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 const App = () => (
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Routes>
+        <Route exact path="/" element={<Movies />} />
+        <Route exact path="/profile/:id" element={<Profile />} />
+        <Route exact path="/movie/:id" element={<MovieInformation />} />
+        <Route exact path="/actors/:id" element={<Actors />} />
+      </Routes>
+    </main>
+  </div>
 );
 
 export default App;
