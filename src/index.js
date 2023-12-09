0d982375d4d9e7d0ebd861274550ac1d2c241a76
DeIT0921 <danarikram@gmail.com>
2023-12-10 01:00:35 +0700

    setup redux and showing movie list

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
@@ -11,9 +13,11 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    </ThemeProvider>,
+  </Provider>,
 );
