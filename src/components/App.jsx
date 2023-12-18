c312eb84f1323f9c0603bfaf1ef7c72432dfad26
new name <danarikram@gmail.com>
2023-12-19 00:21:07 +0700

    fixing bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,10 +16,10 @@ const App = () => {
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Routes>
-          <Route exact path="/" element={<Movies />} />
-          <Route exact path="/profile/:id" element={<Profile />} />
-          <Route exact path="/movie/:id" element={<MovieInformation />} />
-          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route path="/*" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
         </Routes>
       </main>
     </div>
