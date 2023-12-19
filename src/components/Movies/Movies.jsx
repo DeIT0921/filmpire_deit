16c329219d692b7e9321e61a3188ae67dee024c5
new name <danarikram@gmail.com>
2023-12-19 19:41:25 +0700

    implementing Alan-ai features and featured card

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable import/no-cycle */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetMoviesQuery } from '../../services/TMDB';
-import { MovieList, Pagination } from '..';
+import { FeaturedMovie, MovieList, Pagination } from '..';
 
 const Movies = () => {
   const [page, setPage] = useState(1);
@@ -13,7 +12,7 @@ const Movies = () => {
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   if (isFetching) {
     return (
@@ -27,7 +26,7 @@ const Movies = () => {
     return (
       <Box display="flex" alignItems="center" mt="20px">
         <Typography variant="h4">
-          No Movies that match that name.
+          No movies that match that name.
           <br />
           Please search for something else.
         </Typography>
@@ -35,11 +34,12 @@ const Movies = () => {
     );
   }
 
-  if (error) return 'An error has occurred.';
+  if (error) return 'An error has occured.';
 
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+      <FeaturedMovie movie={data.results[0]} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
   );
